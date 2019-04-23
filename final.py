e44a407730b5f1f0f6e3a6228ec3d75760b63eed
Rodney <hotrodsterjr@gmail.com>
2019-04-23 07:35:43 -0700

    added balls and started the "check" method

--- final.py
+++ final.py
@@ -8,8 +8,8 @@ Example
 """
 from graphics import*
 import math
-WIDTH = 654
-HEIGHT = 600
+WIDTH = 654.0
+HEIGHT = 600.0
 def main():
     win = GraphWin("Example", WIDTH,HEIGHT)
     win.yUp()    
@@ -20,21 +20,23 @@ def main():
     win.close()
 
 def drawTable(win):
+    brown = color_rgb(101,53,51)
     x0 = 0
     y0 = HEIGHT/2 - WIDTH*(32/114.0)
     xf = WIDTH
     yf = HEIGHT/2 + (32/114.0)*WIDTH
     rect = Rectangle(Point(x0,y0), Point(xf,yf))
-    rect.setFill("brown")
-    rect.setOutline("brown")
+    rect.setFill(brown)
+    rect.setOutline(brown)
 
+    green = color_rgb(11,135,127)
     x0 = (7/114.0)*WIDTH
     y0 = HEIGHT/2 - WIDTH * (32/114.0) + (7/114.0) * WIDTH
     xf = (107/114.0)*WIDTH
     yf = HEIGHT/2 + (32/114.0) * WIDTH - (7/114.0)*WIDTH
     rect2 = Rectangle(Point(x0,y0),Point(xf,yf))
-    rect2.setFill("green")
-    rect2.setOutline("green")
+    rect2.setFill(green)
+    rect2.setOutline(green)
 
     x = x0
     y = y0
@@ -85,41 +87,77 @@ def createBall(x,y,radius,color,win):
     return ball
 
 def check(balls,velocities,dim):
-    """
     num = 0
+    radius = balls[0].getRadius()
     for ball in balls:
+        x = ball.getCenter().getX()
+        y = ball.getCenter().getY()
+        if x + radius >= dim[2] or x - radius <= dim[0]:
+            velocities[num][0] *= -1
+        if y + radius >= dim[3] or y - radius <= dim[1]:
+            velocities[num][1] *= -1
+            
         for i in range(len(balls)):
+
             if i != num:
-                #check each ball
-                #if in contact then change direction and speed
+                newX = balls[i].getCenter.getX()
+                newY = balls[i].getCenter.getY()
+                distance = ((x-newX)**2 + (y-newY)**2)**0.5
+                #if distance <= radius:
+                    
         num += 1
-    """         
-    x = balls.getCenter().getX()
-    y = balls.getCenter().getY()
-    radius = balls.getRadius()
+        
+    
 
-    if x + radius >= dim[2] or x - radius <= dim[0]:
-        velocities[0] *= -1
-
-    if y + radius >= dim[3] or y - radius <= dim[1]:
-        velocities[1] *= -1
+    
     return velocities
 
-def playGame(win,dim):
-    x = 327
-    y = 300
-    #scratc
+def drawBalls(win):
+    space = 2.0
     radius = (1.125/114)*WIDTH
-    color = "white"
-    ball = createBall(x,y,radius,color,win)
-    v = [3,3]
-    point = win.checkMouse()
+    main = createBall(0.28*WIDTH,HEIGHT/2,radius,"white",win)
+    b1 = createBall(0.72 *WIDTH, HEIGHT/2,radius,"yellow",win)
+    b10 = createBall(b1.getCenter().getX() + 2*radius + space,
+                     HEIGHT/2 + radius + space,radius,"blue",win)
+    b11 = createBall(b10.getCenter().getX(),HEIGHT/2 - radius - space, radius,
+                     "red",win)
+    b6 = createBall(b11.getCenter().getX() + 2*radius +space,
+                    HEIGHT/2 + 2*radius +space, radius,"green",win) #change to green
+    b8 = createBall(b6.getCenter().getX(), HEIGHT/2, radius, "black",win)
+    b9 = createBall(b6.getCenter().getX(), HEIGHT/2 -2*radius - space,
+                    radius,"yellow",win)
+    b5 = createBall(b9.getCenter().getX() + 2*radius + space,
+                    HEIGHT/2 - 3*radius - 2*space,radius,"orange",win)
+    b12 = createBall(b5.getCenter().getX(), HEIGHT/2 - radius - space, radius,
+                     "purple",win)
+    b7 = createBall(b5.getCenter().getX(), HEIGHT/2 +radius + space, radius,
+                    "brown",win)
+    b13 = createBall(b5.getCenter().getX(), HEIGHT/2 + 3*radius +2*space, radius,
+                     "orange",win)
+    b2 = createBall(b5.getCenter().getX() + 2*radius + space,
+                    HEIGHT/2 - 2*radius - space,radius, "blue",win)
+    b3 = createBall(b2.getCenter().getX(), HEIGHT/2 + 2*radius + space, radius,
+                    "red",win)
+    b4 = createBall(b2.getCenter().getX(), HEIGHT/2 + 4*radius + 2*space, radius
+                    ,"purple",win)
+    b14 = createBall(b2.getCenter().getX(), HEIGHT/2, radius, "green", win) #change to green
+    b15 = createBall(b2.getCenter().getX(),HEIGHT/2 - 4*radius - 2*space, radius
+                     ,"brown",win)
+
+    return [main,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15]
+
+def playGame(win,dim):
+    balls = drawBalls(win)
+    velocities = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]
+                  [0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+    radius = (1.125/114)*WIDTH
+    
+    
     while point == None:
         ball.move(v[0],v[1])
         v = check(ball,v,dim)
         time.sleep(1.0/60)
         point = win.checkMouse()
-    
     
 
 if __name__ == "__main__":
